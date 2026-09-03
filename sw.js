87a843614b911a5e63b3d8c03832f68df685aa0e
Ilma Zaqi <redmi.zaqi@gmail.com>
2026-09-03 21:50:46 +0700

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'trs398-pwa-v2';
+const CACHE_NAME = 'trs398-pwa-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -36,4 +38,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
